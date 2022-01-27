36a26689b5d856bdf72d6bb832baa43fd69fe9b0
carobang <93027982+carobang@users.noreply.github.com>
2022-01-27 01:38:29 -0500

    INFO6128 Lab01
    change app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const parsedUrl = new URL(window.location);
     // searchParams.get() will properly handle decoding the values.
-    console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
-    console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
-    console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+   document.getElementById("title").innerHTML= parsedUrl.searchParams.get('title');
+   document.getElementById("text").innerHTML=parsedUrl.searchParams.get('text');
+   document.getElementById("url").innerHTML=parsedUrl.searchParams.get('url');
   });
