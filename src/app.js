5104119949ebdc2d7eee1fab7962eade13a329c3
carobang <93027982+carobang@users.noreply.github.com>
2022-01-27 15:49:56 -0500

    INFO6128 Lab01
    correction

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const parsedUrl = new URL(window.location);
     // searchParams.get() will properly handle decoding the values.
-   document.getElementById("title").innerHTML= parsedUrl.searchParams.get('title');
+   document.getElementById("title").innerHTML=parsedUrl.searchParams.get('title');
    document.getElementById("text").innerHTML=parsedUrl.searchParams.get('text');
    document.getElementById("url").innerHTML=parsedUrl.searchParams.get('url');
   });
