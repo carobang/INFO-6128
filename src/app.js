2c3a8352596f870614590168754c0cbee7c21d0e
carobang <93027982+carobang@users.noreply.github.com>
2022-01-27 15:58:23 -0500

    INFO6128 Lab01
    Modify format

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const parsedUrl = new URL(window.location);
     
-   document.getElementById("title").innerHTML="Titile"+parsedUrl.searchParams.get('title');
-   document.getElementById("text").innerHTML="Text"+parsedUrl.searchParams.get('text');
-   document.getElementById("url").innerHTML="Url"+parsedUrl.searchParams.get('url');
+   document.getElementById("title").innerHTML="Titile: "+parsedUrl.searchParams.get('title');
+   document.getElementById("text").innerHTML="Text: "+parsedUrl.searchParams.get('text');
+   document.getElementById("url").innerHTML="Url: "+parsedUrl.searchParams.get('url');
   });
